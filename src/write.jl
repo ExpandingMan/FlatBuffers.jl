0730f323efbe2a517f0e96e60a4caa797a70fc09
Expanding Man <savastio@protonmail.com>
2020-06-23 20:53:47 -0400

    finally a little less undecided about writing

--- src/write.jl
+++ src/write.jl
@@ -1,9 +1,10 @@
 
+write!(io::IO, x) = write(io, x)
 
 function write!(io::IO, v::AbstractVector{<:BitsType})
     write(io, Int32(length(v))) + sum(x -> write(io, x), v)
 end
-function write!(io::IO, v::AbstractVector{<:AbstractString})
+function write!(io::IO, v:AbstractString)
     write(io, Int32(ncodeunits(v))) + sum(x -> write(io, x), codeunits(v)) + write(io, 0x00)
 end
 
@@ -15,6 +16,41 @@ function write!(io::IO, vt::VTable)
 end
 
 
-function write!(::TableType, io::IO, t)
+mutable struct Writer{T}
+    obj::Ref{T}
+    buffer::IOBuffer
+    position::Union{Int64,Nothing}
+    children::Vector{Union{Writer,Nothing}}  # TODO hoping these pointers are ok and don't kill performance
+end
 
+function Writer(obj, pos::Union{Nothing,Integer}=nothing)
+    Writer{typeof(obj)}(obj, IOBuffer(), pos, children(Writer, obj))
+end
+
+writer(::TableType, obj) = Writer(obj)
+writer(::StructType, obj) = Writer(obj)
+writer(::NoType, obj) = nothing
+writer(obj) = writer(BufferType(typeof(obj)), obj)
+
+children(w::Writer) = w.children
+children(w::Writer, i::Integer) = w.children[i]
+
+children(::Type{Writer}, obj) = children(BufferType(typeof(obj)), obj)
+
+children(::NoType, obj) = Writer[]
+children(::StructType, obj) = Union{Writer,Nothing}[writer(getfield(obj, i)) for i ∈ 1:nfields(obj)]
+children(::TableType, obj) = Union{Writer,Nothing}[writer(getfield(obj, i)) for i ∈ 1:nfields(obj)]
+
+write!(w::Writer{T}) where {T} = write!(BufferType(T), w)
+
+write!(::StructType, w::Writer) = sum(i -> write!(w, i), 1:length(children(w)))
+# TODO obviously this isn't done
+write!(::TableType, w::Writer) = sum(i -> write!(w, i), 1:length(children(w)))
+
+# TODO probably everything in talbe needs to be written to own sub-buffer, to handle alignment and stuff
+
+# TODO may still have to worry about padding here if it's between fields
+function write!(w::Writer, i::Integer)
+    c = children(w, i)
+    isnothing(c) ? write!(w.buffer, getfield(w.obj[], i)) : write!(c)
 end
---
+++ test/write_test.jl
@@ -0,0 +1,8 @@
+include("simple_example2.jl")
+
+x = Vec3(0, 1, 2)
+
+axe = Weapon("Axe", 6)
+
+wx = FB.Writer(x)
+waxe = FB.Writer(axe)
